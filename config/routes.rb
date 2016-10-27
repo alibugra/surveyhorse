296676a6c1c2f6aaaee9e6eb0d673e929a3b5d6e
BurakSA <buraksaglm@gmail.com>
2016-10-27 20:09:20 +0300

    Home controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources 'home'
+  get 'home/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
