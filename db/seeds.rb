4163646f60f9d3a3dccf6a2fafddd7370fe19fcc
alibugra <alibugra@alibugra.com>
2016-12-03 20:53:04 +0300

    System admin login process tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-User.create(username: `admin`, password: `123456`, email: `admin@surveyhorse.com`, role: 3)
+User.create(username: 'admin', password: '123456', email: 'admin@surveyhorse.com', role: 3)
