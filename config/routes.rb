d44cd8cf1dec3e96e2884f20877340bcc4b9e517
alibugra <alibugra@alibugra.com>
2016-12-15 19:26:44 +0300

    Routes modified according to new controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,29 @@
 Rails.application.routes.draw do
+  # SYSTEM ADMIN PAGES
+  namespace :sysadmin do
 
-  get 'faq/index'
+  end
 
-  get 'contact/index'
+  # SURVEY MANAGER PAGES
+  namespace :surveymanager do
+
+  end
+
+  # SURVEYOR PAGES
+  namespace :surveyor do
+
+  end
+
+  # MAIN PAGES
+  get '/home' => 'home#index'
+  get '/faq' => 'faq#index'
+  get '/contact' => 'contact#index'
 
   # AUTHENTICATION
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
   post '/sessions' => 'sessions#create'
   delete '/sessions', to: 'sessions#destroy', as: 'sign_out'
-  # MAIN PAGES
-  get '/home' => 'home#index'
 
   root to: 'home#index'
 
