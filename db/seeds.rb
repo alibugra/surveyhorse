a409c44c9566cecdb45ac88fd3b8f7fe70731d09
BurakSA <buraksaglm@gmail.com>
2016-12-15 19:24:46 +0300

    Model seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,5 @@
 User.create(username: 'admin', password: '123456', email: 'admin@surveyhorse.com', role: 3)
+
+# Role.create(name: 'System Admin', role_id: 3)
+Role.create(name: 'Survey Manager', role_id: 2)
+Role.create(name: 'Surveyor', role_id: 1)
