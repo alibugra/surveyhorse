3432eac2df07ce39ab3e0e2790f8c24448050c3c
alibugra <alibugra@alibugra.com>
2016-12-22 17:22:48 +0300

    Routes file updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 Rails.application.routes.draw do
   # SYSTEM ADMIN PAGES
   namespace :sysadmin do
-
+    get '/users' => 'users#index'
+    post '/delete-user' => 'users#delete_user'
+    get '/users/:id/edit', to: 'users#edit' , as: 'edit_user'
+    post '/users/:id' , to: 'users#update' , as: 'update_user'
   end
 
   # SURVEY MANAGER PAGES
   namespace :surveymanager do
-
+    get '/surveys' => 'surveys#index'
+    get '/create' => 'surveys#create'
+    post '/create' => 'surveys#create'
+    get '/profile' => 'profile#index'
+    post '/profile' => 'profile#edit_profile'
   end
 
   # SURVEYOR PAGES
   namespace :surveyor do
-
+    get '/profile' => 'profile#index'
+    post '/profile' => 'profile#edit_profile'
   end
 
   # MAIN PAGES
