d4dd122e43f15ba559bc80ea5c10206fabffc518
emresasmaz <sasmazemre7@gmail.com>
2016-12-27 13:24:28 +0300

    Surveyor search controller added

---
+++ app/controllers/surveyor/search_controller.rb
@@ -0,0 +1,14 @@
+class Surveyor::SearchController < ApplicationController
+  before_action :require_surveyor
+  def index
+    @surveys = Survey.all.order('id desc').search("-99999999999999")
+    if params[:search]
+      @surveys = Survey.all.order('id desc').search(params[:search])
+      if @surveys.count < 1
+        flash[:notice] = "No_any_result"
+      else
+        flash[:notice] = "..."
+      end
+    end
+  end
+end
