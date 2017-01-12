71cb672af3499e1c7170583c75e15f3db1d02c15
alibugra <alibugra@alibugra.com>
2017-01-12 22:32:35 +0300

    Surveyor page, search and surveys modified

--- app/controllers/surveyor/search_controller.rb
+++ app/controllers/surveyor/search_controller.rb
@@ -3,9 +3,11 @@ class Surveyor::SearchController < ApplicationController
   def index
     @surveys = Survey.all.order('id desc').search("-99999999999999")
     if params[:search]
-      @surveys = Survey.all.order('id desc').search(params[:search])
+      @surveys = Survey.all.where(status: "Public", is_published: "Yes").order('id desc').search(params[:search])
       if @surveys.count < 1
         flash[:notice] = "No_any_result"
+      else
+        flash[:notice] = "..."
       end
     end
   end
